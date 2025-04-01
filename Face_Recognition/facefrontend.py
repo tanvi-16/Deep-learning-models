7956e5b7b17e306f376c2a8fc5fe98b614288c55
Tanvi Bhatt <tanvibhatt@vebuin.com>
2025-04-01 10:06:38 +0530

    first commit

--- Face_Recognition/facefrontend.py
+++ Face_Recognition/facefrontend.py
@@ -1,4 +1,3 @@
-# Importing the libraries
 from PIL import Image
 from keras.applications.vgg16 import preprocess_input
 import base64
@@ -12,41 +11,29 @@ import numpy as np
 from keras.preprocessing import image
 model = load_model('facefeatures_model.h5')
 
-# Loading the cascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def face_extractor(img):
-    # Function detects faces and returns the cropped face
-    # If no face detected, it returns the input image
-    
-    #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(img, 1.3, 5)
     
     if faces is ():
         return None
     
-    # Crop all faces found
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)
         cropped_face = img[y:y+h, x:x+w]
 
     return cropped_face
 
-# Doing some Face Recognition with the webcam
 video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
-    #canvas = detect(gray, frame)
-    #image, face =face_detector(frame)
     
     face=face_extractor(frame)
     if type(face) is np.ndarray:
         face = cv2.resize(face, (224, 224))
         im = Image.fromarray(face, 'RGB')
-        #Resizing into 128x128 because we trained the model with this image size.
         img_array = np.array(im)
-        #Our keras model used a 4D tensor, (images x height x width x channel)
-        #So changing dimension 128x128x3 into 1x128x128x3 
         img_array = np.expand_dims(img_array, axis=0)
         pred = model.predict(img_array)
         print(pred)
